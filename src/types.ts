6fbca8d6f34e6947a4f1b8abc93da85930d6ede6
enes-dogan <enesdogan210@gmail.com>
2023-11-25 01:35:19 +0300

    GameBoard component and initialGamaboard created

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,10 @@ export interface WinningCombinations {
   [index: number]: WinningCombination;
 }
 
+export interface initialGameBoardType {
+  [rowIndex: number]: Array<string | null>;
+}
+
 export interface PlayerProps {
   initialName: string;
   symbol: string;
